62f8e58ab1b1fe4dd8e75dd1550ae319af0c0aa7
louis091 <louis.phammm@gmail.com>
2024-01-24 15:56:25 +0700

    10% of StudentManagement

---
+++ src/Connectivity/Data.java
@@ -0,0 +1,17 @@
+package Connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Data {
+    public static  Connection getCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc://mysql://localhost/students", "root", "12345");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
